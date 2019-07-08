e74b98c9ee10046bd20a25cbe2c2be8852e0fe5a
Masa Sato <masaocsato@yahoo.com>
2019-07-07 19:11:03 -0700

    basic layout, no functionality yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Countdown from "react-countdown-now";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Countdown date={Date.now() + 10000} />
+      </div>
+      <div>
+        <button type="button" onClick={this.resetTimer()}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
